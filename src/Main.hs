c9a2eb80be4bd65ad098f864b0211ed16e37f6d2
Ulf Sauer <ulf.sauer@gmail.com>
2012-07-16 22:12:43 +0200

    Added Trampolines (A..I) and Targets (1..9) - like one-time teleporters from Trampoline to Target

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
-import Prelude hiding (lookup, filter)
-import Data.List hiding (lookup, insert, filter)
-import Data.Map hiding (map)
+import Prelude hiding (lookup, filter, null)
+import qualified Prelude as P
+import Data.List hiding (lookup, insert, filter, null)
+import Data.Map hiding (map, foldl)
 import Control.Monad.State
 import Control.Concurrent (threadDelay)
 import System.IO (BufferMode(NoBuffering),stdin, hSetBuffering, hSetEcho)
@@ -25,15 +26,20 @@ data Object
         | LiftOpen
         | LiftClosed
         | Earth
+        | Trampoline Char
+        | Target Char
         | Empty
-        deriving Eq
+        deriving (Eq, Ord)
 
 instance Show Object where
         show = (:[]). objectToChar
 
 type Position = (Integer, Integer)
-type Level = Map Position Object
 
+data Level = Level
+        { lvMap :: Map Position Object
+        , lvTrampolines :: Map Object Object -- Trampoline -> Target
+        }
 
 data Movement
         = MvLeft
@@ -62,11 +68,13 @@ data GameState = GameState
         , gsRobotPosition       :: Position
         , gsLiftPosition        :: Position
         
+        , gsTargets             :: Map Object Position   -- Target -> Position of Target
+        , gsTargetSources       :: Map Object [Position] -- Target -> [Position of Trampoline]
+        
         , gsProgress            :: GameProgress
         , gsLambdasCollected    :: Integer
         , gsMoves               :: Integer
         }
-
 
 -- Functions
 
@@ -80,35 +88,78 @@ objectToChar o
                 LiftClosed      -> 'L'
                 LiftOpen        -> 'O'
                 Earth           -> '.'
+                Trampoline c    -> c
+                Target c        -> c
                 Empty           -> ' ' -- TODO: eww? see note below
 
 
 charToObject :: Char -> Object
 charToObject c
         = case c of
-                'R'  -> Robot
-                '#'  -> Wall
-                '*'  -> Rock
-                '\\' -> Lambda
-                'L'  -> LiftClosed
-                'O'  -> LiftOpen
-                '.'  -> Earth
-                ' '  -> Empty
-                _    -> error $ "Cannot convert \"" ++ c : "\" to Object: no mapping found"
-
+                'R'                     -> Robot
+                '#'                     -> Wall
+                '*'                     -> Rock
+                '\\'                    -> Lambda
+                'L'                     -> LiftClosed
+                'O'                     -> LiftOpen
+                '.'                     -> Earth
+                ' '                     -> Empty
+                a | a `elem` ['A'..'I'] -> Trampoline a
+                  | a `elem` ['0'..'9'] -> Target a
+                a   -> error $ "Cannot convert \"" ++ a : "\" to Object: no mapping found"
 
 readLevelFromFile :: FilePath -> IO Level
 readLevelFromFile f = do
         c  <- readFile f
-        return . levelStringToMap . lines $ c
+        let (lvlString, lvlMetadata) = splitLevelAndMetadataString . lines $ c
+        let lMap = levelStringToMap lvlString
+        let lTrampolines = extractTrampolinesFromMetadata empty lvlMetadata
+
+        return Level { lvMap = lMap
+                     , lvTrampolines = lTrampolines}
+        where
+        splitLevelAndMetadataString :: [String] -> ([String], [String])
+        splitLevelAndMetadataString s = (tFst, P.filter (/= "") tSnd)
+                where (tFst, tSnd) = span (/= "") s
+        -- TODO: quick and dirty parsing :(
+        extractTrampolinesFromMetadata :: Map Object Object -> [String] -> Map Object Object
+        extractTrampolinesFromMetadata tMap []           = tMap
+        extractTrampolinesFromMetadata tMap (l:ls) =
+                if "Trampoline " `isPrefixOf` l
+                  then
+                        extractTrampolinesFromMetadata (insert (Trampoline trampo) (Target target) tMap) ls
+                  else
+                        tMap
+                where
+                (trampo:target:_) = P.filter (`elem` ['A' .. 'I'] ++ ['0'..'9']) l
+{-
+        extractTrampolinesFromMetadata tMap (l:ls) =
+                if "Trampoline " `isPrefixOf` l
+                  then
+                        extractTrampolinesFromMetadata (foldl (flip (insert trampo)) tMap targets) ls
+                  else
+                        tMap
+                where
+                (trampo:targets) = P.filter (`elem` ['A' .. 'I'] ++ ['0'..'9']) l
+-}
 
 
-levelStringToMap :: [String] -> Level
+levelStringToMap :: [String] -> Map Position Object
 levelStringToMap sl = fromList . concatMap (\(y, s) -> zip [(x,y) | x <- [1..]] (map charToObject s)) $ zip [1..] (reverse sl)
 
 
 printLevel :: Level -> IO ()
-printLevel = sequence_ . printAList . levelToSortedAList
+printLevel l = do
+        unless (null trams) $ do
+                putStrLn "Trampolines:"
+                mapM_ (putStrLn . show') $ toList trams
+        printLevelMap l
+        where
+        trams = lvTrampolines l
+        show' (tram, targ) = show tram ++ " -> " ++ show targ
+
+printLevelMap :: Level -> IO ()
+printLevelMap = sequence_ . printAList . levelToSortedAList
         where
         printAList :: [(Position, Object)] -> [IO ()]
         printAList ls@(((x0,_),o):((x1,_),_):_)
@@ -118,7 +169,7 @@ printLevel = sequence_ . printAList . levelToSortedAList
         printAList [] = [putStrLn ""]
 
         levelToSortedAList :: Level -> [(Position, Object)]
-        levelToSortedAList = sortBy levelMapOutputSort . toList
+        levelToSortedAList = sortBy levelMapOutputSort . toList . lvMap
         
         levelMapOutputSort ((x0,y0),_) ((x1,y1),_)
                 | y0 < y1       = GT
@@ -140,15 +191,41 @@ createGame lvl = do
                 , gsRobotPosition       = roboPos
                 , gsLiftPosition        = liftPos
                 
+                , gsTargets             = revertMap targs
+                , gsTargetSources       = targetSourcePositions
+                
                 , gsProgress            = Running
                 , gsLambdasCollected    = 0
                 , gsMoves               = 0
                 }
         where
-        robos = filter (== Robot)      lvl
-        lifts = filter (== LiftClosed) lvl
+        lmap   = lvMap lvl
+        robos = filter (== Robot)      lmap
+        trams = filter isTrampoline    lmap
+        targs = filter isTarget        lmap
+        lifts = filter (== LiftClosed) lmap
         lvlDims = (maximum xs, maximum ys)
-        (xs,ys) = unzip . keys $ lvl
+        (xs,ys) = unzip . keys $ lmap
+        isTrampoline (Trampoline _)     = True
+        isTrampoline _                  = False
+        isTarget (Target _)             = True
+        isTarget _                      = True
+        targetSourcePositions = listMap . filterMaybes . map (\(pos,obj) -> (pos, lookup obj (revertMap trams)) ) . toList $ lvTrampolines lvl
+
+
+revertMap :: Ord b => Map a b -> Map b a
+revertMap = fromList . map flipElems . toList
+        where flipElems (a,b) = (b,a)
+
+
+listMap :: Ord k => [(k, a)] -> Map k [a]
+listMap pairs =
+    foldl insertPair empty (reverse pairs)
+    where insertPair m (k,a) = insert k (a : findWithDefault [] k m) m
+
+
+filterMaybes :: [(a, Maybe b)] -> [(a, b)]
+filterMaybes ls = [(x,y) | (x, Just y) <- ls]
 
 
 updateGameState :: GameState -> GameState
@@ -165,7 +242,7 @@ updateGameState gs = execState (updateLevel' keysToUpdate gs) gs
        
 updateLevelByPosition :: GameState -> GameState -> Position -> GameState
 updateLevelByPosition gs gs' pos
-        = case lookup pos (gsLevel gs) of
+        = case lookup pos (lvMap . gsLevel $ gs) of
                 Nothing -> gs'
                 Just e -> processObject gs gs' e pos
 
@@ -174,29 +251,29 @@ processObject :: GameState -> GameState -> Object -> Position -> GameState
 processObject gs gs' o (x,y)
         = gs'   { gsLevel = case o of
                 Rock    | lookup (x, y-1)     lvl == Just Empty
-                        -> insert (x, y-1) Rock . insert (x,y) Empty $ lvl'
+                        -> (gsLevel gs') { lvMap = insert (x, y-1) Rock . insert (x,y) Empty $ lvl'}
                 Rock    | lookup (x, y-1)     lvl == Just Rock &&
                           lookup (x+1, y)     lvl == Just Empty &&
                           lookup (x+1, y-1)   lvl == Just Empty
-                        -> insert (x+1, y-1) Rock . insert (x,y) Empty $ lvl'
+                        -> (gsLevel gs') { lvMap = insert (x+1, y-1) Rock . insert (x,y) Empty $ lvl'}
                 Rock    | lookup (x, y-1)     lvl == Just Rock &&
                           ( lookup (x+1, y)   lvl /= Just Empty ||
                             lookup (x+1, y-1) lvl /= Just Empty
                           ) &&
                           lookup (x-1, y)     lvl == Just Empty &&
                           lookup (x-1, y-1)   lvl == Just Empty
-                        -> insert (x-1, y-1) Rock . insert (x,y) Empty $ lvl'
+                        -> (gsLevel gs') { lvMap = insert (x-1, y-1) Rock . insert (x,y) Empty $ lvl'}
                 Rock    | lookup (x, y-1)     lvl == Just Lambda &&
                           lookup (x+1, y)     lvl == Just Empty &&
                           lookup (x+1, y-1)   lvl == Just Empty
-                        -> insert (x+1, y-1) Rock . insert (x,y) Empty $ lvl'
+                        -> (gsLevel gs') { lvMap = insert (x+1, y-1) Rock . insert (x,y) Empty $ lvl'}
                 LiftClosed | foldr' ((&&) . (/=Lambda)) True lvl
-                        -> insert (x,y) LiftOpen lvl'
-                _       -> lvl'
+                        -> (gsLevel gs') { lvMap = insert (x,y) LiftOpen lvl'}
+                _       -> (gsLevel gs') { lvMap = lvl'}
                 }
         where
-        lvl = gsLevel gs
-        lvl' = gsLevel gs'
+        lvl  = lvMap . gsLevel $ gs
+        lvl' = lvMap . gsLevel $ gs'
 
 
 playGame :: Int -> GameState -> IO GameState
@@ -229,8 +306,8 @@ checkIfRobotGotCrushed oldGs newGs
             else
                 newGs
         where
-        aboveOld        = lookup (rX,rY+1) $ gsLevel oldGs
-        aboveNew        = lookup (rX,rY+1) $ gsLevel newGs
+        aboveOld        = lookup (rX,rY+1) $ lvMap . gsLevel $ oldGs
+        aboveNew        = lookup (rX,rY+1) $ lvMap . gsLevel $ newGs
         (rX,rY)         = gsRobotPosition oldGs
 
 
@@ -254,37 +331,48 @@ moveRobot :: GameState -> Movement -> Maybe GameState -- TODO: Maybe unnecessary
 moveRobot game dir = do
         let lvl = gsLevel game
         nrp   <- newRobotPosition
-        field <- lookup nrp lvl
+        field <- lookup nrp $ lvMap lvl
+        let lmap = lvMap lvl
         case field of
                 Robot   | dir == MvWait
                                 -> return game
                 Robot   | dir == MvAbort
                                 -> return game  { gsProgress = Abort}
-                Empty           -> return game  { gsLevel = insert nrp Robot . insert orp Empty $ lvl
+                Empty           -> return game  { gsLevel = (gsLevel game) { lvMap = insert nrp Robot . insert orp Empty $ lmap}
                                                 , gsRobotPosition = nrp
                                                 , gsMoves = gsMoves game + 1}
-                Earth           -> return game  { gsLevel = insert nrp Robot . insert orp Empty $ lvl
+                Earth           -> return game  { gsLevel = (gsLevel game) { lvMap = insert nrp Robot . insert orp Empty $ lmap}
                                                 , gsRobotPosition = nrp, gsMoves = gsMoves game + 1}
-                Lambda          -> return game  { gsLevel = insert nrp Robot . insert orp Empty $ lvl
+                Lambda          -> return game  { gsLevel = (gsLevel game) { lvMap = insert nrp Robot . insert orp Empty $ lmap}
                                                 , gsLambdasCollected = gsLambdasCollected game + 1
                                                 , gsRobotPosition = nrp
                                                 , gsMoves = gsMoves game + 1}
-                LiftOpen        -> return game  { gsLevel = insert nrp Robot . insert orp Empty $ lvl
+                LiftOpen        -> return game  { gsLevel = (gsLevel game) { lvMap = insert nrp Robot . insert orp Empty $ lmap}
                                                 , gsProgress = Win
                                                 , gsRobotPosition = nrp
                                                 , gsMoves = gsMoves game + 1}
                 Rock    |  dir == MvRight
-                        && lookup (rX+2, rY) lvl == Just Empty
-                                -> return game  { gsLevel = insert nrp Robot . insert (rX+2, rY) Rock . insert orp Empty $ lvl
+                        && lookup (rX+2, rY) (lvMap lvl) == Just Empty
+                                -> return game  { gsLevel = (gsLevel game) { lvMap = insert nrp Robot . insert (rX+2, rY) Rock . insert orp Empty $ lmap}
                                                 , gsRobotPosition = nrp
                                                 , gsMoves = gsMoves game + 1}
                 Rock    |  dir == MvLeft
-                        && lookup (rX-2, rY) lvl == Just Empty
-                                -> return game  { gsLevel = insert nrp Robot . insert (rX-2, rY) Rock . insert orp Empty  $ lvl
+                        && lookup (rX-2, rY) (lvMap lvl) == Just Empty
+                                -> return game  { gsLevel = (gsLevel game) { lvMap = insert nrp Robot . insert (rX-2, rY) Rock . insert orp Empty  $ lmap}
                                                 , gsRobotPosition = nrp
                                                 , gsMoves = gsMoves game + 1}
+                tc@(Trampoline _)
+                                -> return game  { gsLevel = (gsLevel game) { lvMap = insert roboPosTrampoline Robot . insert nrp Empty. insert orp Empty $ lmap}
+                                                , gsRobotPosition = roboPosTrampoline
+                                                , gsMoves = gsMoves game + 1}
+                        where
+                        roboPosTrampoline = unMaybe $ do
+                                targ <- lookup tc (lvTrampolines (gsLevel game))
+                                lookup targ (gsTargets game)
                 _               -> Nothing
         where
+        unMaybe (Just a) = a
+        unMaybe _        = error "unexpected Nothing value -> Trampoline may not have a Target" -- TODO: better error handling
         newRobotPosition= case dir of
                 MvUp            -> Just (rX   ,rY+1)
                 MvLeft          -> Just (rX-1 ,rY  )
@@ -325,6 +413,7 @@ printControls = do
         putStrLn "  R to restart"
         putStrLn "  N to skip the level"
         putStrLn "  Q to quit"
+        putStrLn ""
 
 -- Main
 
